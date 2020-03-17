908621efec87a55d4cc4e74aca3f8232dd265b22
Erwin Manurung <e.manurung@allegrodevelopment.com>
2020-03-17 15:01:46 +0700

    initial project using antd framework

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Heading from './Heading.jsx';
+import {Button, DatePicker, version} from "antd";
+import "antd/dist/antd.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React!!!
-        </a>
+        <Heading hello="Learn to React and do React!!!"></Heading>
+        <h1>antd version: {version}</h1>
       </header>
+      <DatePicker style={{ marginLeft: 8, marginBottom: 8 }}/>
+        <Button type="primary" style={{ marginLeft: 8 }}>
+          Primary Button
+        </Button>
+
     </div>
   );
 }
---
+++ src/Heading.jsx
@@ -0,0 +1,11 @@
+import React, {Component} from "react";
+
+class Heading extends Component{
+
+    render(){
+        return (<h2>{this.props.hello}</h2>);
+    };
+
+}
+
+export default Heading;
